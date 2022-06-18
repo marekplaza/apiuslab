c188893efa9536109a9acddb260dba4154a4c8d2
marekplaza <marekplaza@gmail.com>
2022-06-18 21:34:02 +0000

    start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo "🔥 TIP: menu + enter = powrót do Menu Głównego, exit = powrót do poprzedniego Menu"
+echo "alias menu='/usr/bin/python3 start.py'" >> ~/.bashrc
+echo "export PS1='[🐳+🧪 ApiusLAB ]# '" >> ~/.bashrc
+/bin/bash
+
